8129d4edbcffa79e2945c0c91de954831325840a
Ruixu Geng(耿瑞旭) <gengruixv@163.com>
2018-09-02 20:10:35 +0800

    将m文件改为函数文件以方便调用

--- confocal_nlos_code/Three_D.m
+++ confocal_nlos_code/Three_D.m
@@ -1,7 +1,9 @@
-figure
-v = 1-vol;
-h = vol3d('cdata',v,'texture','2D');
-view(3); 
-% % Update view since 'texture' = '2D'
-vol3d(h);  colormap('gray')
-alphamap('rampdown'), alphamap('decrease'), alphamap('decrease')
+function Three_D(data)
+    figure
+    v = data;
+    h = vol3d('cdata',v,'texture','2D');
+    view(3); 
+    % % Update view since 'texture' = '2D'
+    vol3d(h);  colormap('gray')
+    alphamap('rampdown'), alphamap('decrease'), alphamap('decrease')
+end
